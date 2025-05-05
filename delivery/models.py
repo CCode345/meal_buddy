29677b1230d93dc04362ab6ca4780014f6ced700
CCode345 <chandanbhardwaj914@gmail.com>
2025-05-05 19:31:59 +0530

    3rd commit

--- delivery/models.py
+++ delivery/models.py
@@ -16,4 +16,17 @@ class Restaurant(models.Model):
     'ges&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCODI-v74io0DFQAAAAAdAAAAABAE')
     cuisine = models.CharField(max_length=200)
     rating = models.FloatField()
+
+class Item(models.Model):
+
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name = "items")
+
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    price = models.FloatField()
+    is_veg = models.BooleanField(default=True)
+    picture = models.URLField(max_length=200, default='https://www.google.com/url?sa=i'
+    '&url=https%3A%2F%2Fwww.theodysseyrestaurant.com%2F&psi' \
+    'g=AOvVaw2gyu0awhbj5ADPjOBtjQAd&ust=1746486931934000&source=ima' \
+    'ges&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCODI-v74io0DFQAAAAAdAAAAABAE')
     
